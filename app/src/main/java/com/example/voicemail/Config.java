927c42b96cc6f12a7eb34933275d95c11954e4d4
Vyshnavi Mandadi <vyshnavimandadi@Vyshnavis-MacBook-Air.local>
2022-05-04 13:54:48 +0530

    first commit

---
+++ app/src/main/java/com/example/voicemail/Config.java
@@ -0,0 +1,6 @@
+package com.example.voicemail;
+
+public class Config {
+    public static String EMAIL ="vyshumandadi09@gmail.com";  //gmail address
+    public static String PASSWORD ="Vyshupapa@923"; //password
+}
